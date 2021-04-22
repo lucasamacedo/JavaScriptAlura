0d8d4826cad3b34a5a73a4d2c65271a4628135c6
lucasamacedo <lucas_am_@hotmail.com>
2021-04-22 16:15:28 -0300

    Adicionando operação de embarque

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,20 +6,36 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 17;
+const idadeComprador = 16;
 const estaAcompanhado = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhado) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1, 1);
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1);
+// } else if (estaAcompanhado) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1);
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhado) {
+    console.log("Boa Viagem");
+    listaDeDestinos.splice(2, 1);
 } else {
     console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+
+if (idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem!");
+} else {
+    console.log("Você não pode embarcar");
 }
 
 console.log(listaDeDestinos);
