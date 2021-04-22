8464620777bdaf9e9eebf21ff2ef9c8f7f027a05
lucasamacedo <lucas_am_@hotmail.com>
2021-04-22 10:34:18 -0300

    Primeiro programa em JS

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,14 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+console.log("idade: " + idade);
+
+const idadeSomada = 26+2;
+
+console.log(idadeSomada);
