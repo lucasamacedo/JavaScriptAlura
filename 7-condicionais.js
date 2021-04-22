3884399db65400e162c929a2e7efc39609048224
lucasamacedo <lucas_am_@hotmail.com>
2021-04-22 16:04:22 -0300

    Adicionando opção de estar acompanhado ao comprador menor de idade

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,22 +6,26 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 18;
+const idadeComprador = 17;
+const estaAcompanhado = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
+if (idadeComprador >= 18) {
     console.log("Comprador maior de idade");
     listaDeDestinos.splice(1, 1);
-}else{
+} else if (estaAcompanhado) {
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1, 1);
+} else {
     console.log("Não é maior de idade e não posso vender");
 }
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador == 18);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador == 18);
