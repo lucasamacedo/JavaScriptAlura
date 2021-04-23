9ba55f183e85dba418954bfdee78dcab3d6b9b05
lucasamacedo <lucas_am_@hotmail.com>
2021-04-23 10:10:03 -0300

    Adicionando classe ContaCorrente e operação de Saque

--- C2_Orientacao_a_Objetos/index.js
+++ C2_Orientacao_a_Objetos/index.js
@@ -1,22 +1,37 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valorSacado){
+            this.saldo -= valorSacado;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Lucas";
 cliente1.cpf = 1112233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 8882233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteLucas = new ContaCorrente();
+contaCorrenteLucas.agencia = 1001;
+contaCorrenteLucas.saldo = 0;
+console.log(contaCorrenteLucas.saldo);
+contaCorrenteLucas.saldo += 100;
+console.log(contaCorrenteLucas.saldo);
+
+let valorSacado = 75;
+contaCorrenteLucas.sacar(valorSacado);
+console.log(contaCorrenteLucas.saldo);
 
 console.log(cliente1);
 console.log(cliente2);
