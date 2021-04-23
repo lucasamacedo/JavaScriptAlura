630afed649047aa5df9192362a22f60aa20ff84e
lucasamacedo <lucas_am_@hotmail.com>
2021-04-23 15:49:00 -0300

    Copiando projeto do curso 2 para o curso 3

---
+++ C3_POO_Interfaces_e_Herancas/index.js
@@ -0,0 +1,17 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+
+const cliente1 = new Cliente("Lucas", 1112233309);
+
+const cliente2 = new Cliente("Alice", 8882233309);
+
+const contaCorrenteLucas = new ContaCorrente(cliente1, 1001);
+contaCorrenteLucas.depositar(1000);
+
+const conta2 = new ContaCorrente(cliente2, 102);
+
+contaCorrenteLucas.transferir(100, conta2);
+
+// console.log(contaCorrenteLucas);
+// console.log(conta2);
+console.log(ContaCorrente.numeroDeContas);
