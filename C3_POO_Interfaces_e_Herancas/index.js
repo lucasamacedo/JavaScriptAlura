8692ad5bf44f30a23323a84a059d3d5b63d96ec0
lucasamacedo <lucas_am_@hotmail.com>
2021-04-23 16:22:54 -0300

    Adicionado classe Conta (sem heranca ainda)

--- C3_POO_Interfaces_e_Herancas/index.js
+++ C3_POO_Interfaces_e_Herancas/index.js
@@ -1,14 +1,15 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Lucas", 1112233309);
 
-const contaCorrenteLucas = new ContaCorrente(cliente1, 1001);
+const contaCorrenteLucas = new Conta(0, cliente1, 1001);
 contaCorrenteLucas.depositar(1000);
 contaCorrenteLucas.sacar(100);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteLucas);
