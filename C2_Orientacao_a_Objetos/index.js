942669db52313ed21778c8de6dd0761dbf839946
lucasamacedo <lucas_am_@hotmail.com>
2021-04-23 09:30:40 -0300

    Começando POO em JS

---
+++ C2_Orientacao_a_Objetos/index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "Lucas";
+const cliente1CPF = 1112233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 8882233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
