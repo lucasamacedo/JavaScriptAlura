155513dfaa88e7dbdb8bf49989acfe07f533061c
lucasamacedo <lucas_am_@hotmail.com>
2021-04-26 12:50:35 -0300

    Criando sistema de autenticação muito simples

--- C3_POO_Interfaces_e_Herancas/index.js
+++ C3_POO_Interfaces_e_Herancas/index.js
@@ -1,19 +1,13 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./Conta/ContaCorrente.js"
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Lucas", 1112233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const contaCorrenteLucas = new ContaCorrente(cliente1, 1001);
-contaCorrenteLucas.depositar(1000);
-contaCorrenteLucas.sacar(100);
+const estaLogado = SistemaAutenticacao.login(gerente, "123");
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario);
+console.log(estaLogado);
