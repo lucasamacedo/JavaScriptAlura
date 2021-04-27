d31c8917c954e8d1b43eed6d383be91d53bddd6b
lucasamacedo <lucas_am_@hotmail.com>
2021-04-27 11:08:06 -0300

    Calculando IMC para todos os pacientes da tabela

--- C4_JavaScript_para_Web/js/principal.js
+++ C4_JavaScript_para_Web/js/principal.js
@@ -1,25 +1,33 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+//var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+var paciente;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-var IMC = peso / (altura * altura);
-
-if(peso <= 0 || peso >= 1000){
-    console.log("Valor de peso de paciente inválido!");
-    tdImc.textContent = "Peso inválido";
-} else if(altura <= 0 || altura >= 3.00){
-    console.log("Valor de altura do paciente inválido!");
-    tdImc.textContent = "Altura inválida";
+for(var i = 0; i < pacientes.length; i++){
+    paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    var IMC = peso / (altura * altura);
+    
+    if(peso <= 0 || peso >= 1000){
+        console.log("Valor de peso de paciente inválido!");
+        tdImc.textContent = "Peso inválido";
+    } else if(altura <= 0 || altura >= 3.00){
+        console.log("Valor de altura do paciente inválido!");
+        tdImc.textContent = "Altura inválida";
+    }
+    else{
+        var tdImc = paciente.querySelector(".info-imc").textContent = IMC.toFixed(2);
+    }
 }
-else{
-    var tdImc = paciente.querySelector(".info-imc").textContent = IMC;
-}
+
+
 
 //console.log(IMC);
