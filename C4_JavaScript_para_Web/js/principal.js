229f6952bccd51e347db5baf8f385e020e5ac13c
lucasamacedo <lucas_am_@hotmail.com>
2021-04-27 10:24:39 -0300

    Calculando IMC do primeiro paciente

--- C4_JavaScript_para_Web/js/principal.js
+++ C4_JavaScript_para_Web/js/principal.js
@@ -1,4 +1,13 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = document.querySelector("#primeiro-paciente").querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var IMC = peso / (altura * altura);
+document.querySelector("#primeiro-paciente").querySelector(".info-imc").textContent = IMC;
+//console.log(IMC);
