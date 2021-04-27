9cfab381fc55535162de3cb8e6c131630c775f0f
lucasamacedo <lucas_am_@hotmail.com>
2021-04-27 10:49:52 -0300

    Adicionando validação no cálculo do IMC

--- C4_JavaScript_para_Web/js/principal.js
+++ C4_JavaScript_para_Web/js/principal.js
@@ -5,9 +5,21 @@ var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = document.querySelector("#primeiro-paciente").querySelector(".info-altura");
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var tdImc = paciente.querySelector(".info-imc");
 var IMC = peso / (altura * altura);
-document.querySelector("#primeiro-paciente").querySelector(".info-imc").textContent = IMC;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Valor de peso de paciente inválido!");
+    tdImc.textContent = "Peso inválido";
+} else if(altura <= 0 || altura >= 3.00){
+    console.log("Valor de altura do paciente inválido!");
+    tdImc.textContent = "Altura inválida";
+}
+else{
+    var tdImc = paciente.querySelector(".info-imc").textContent = IMC;
+}
+
 //console.log(IMC);
