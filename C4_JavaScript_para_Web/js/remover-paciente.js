f92eef5d78ec3cf395647c757969726c4f35c2e3
lucasamacedo <lucas_am_@hotmail.com>
2021-04-28 15:05:43 -0300

    Removendo pacientes adicionados e Adicionando efeito de remoção

--- C4_JavaScript_para_Web/js/remover-paciente.js
+++ C4_JavaScript_para_Web/js/remover-paciente.js
@@ -1,7 +1,14 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        this.remove();
-    });
+var tabelaPacientes = document.querySelector("#tabela-pacientes");
+
+tabelaPacientes.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover
+
+    paiDoAlvo.classList.add("fadeOut");
+    setTimeout(function(){
+        paiDoAlvo.remove();
+    }, 500);
+    
 });
