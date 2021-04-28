3415f25d0d4167d0188bf394a4b2cbf5a5a707c6
lucasamacedo <lucas_am_@hotmail.com>
2021-04-28 15:58:27 -0300

    Filtrando tabela dinamicamente

--- C4_JavaScript_para_Web/js/filtra.js
+++ C4_JavaScript_para_Web/js/filtra.js
@@ -2,8 +2,20 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
     var pacientes = document.querySelectorAll(".paciente");
-    pacientes.forEach(function(paciente){
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
-    });
+
+    if(this.value.length > 0){
+        pacientes.forEach(function(paciente){
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            if(!nome.includes(campoFiltro.value)){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        });
+    } else{
+        pacientes.forEach(function(paciente){
+            paciente.classList.remove("invisivel");
+        })
+    }
 });
